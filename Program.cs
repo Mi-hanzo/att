4d448fcbd217f72711516af824477d9fae6a741e
Mi-hanzo <mihailmig@gmail.com>
2024-03-19 16:03:05 +0300

    create void1

--- Program.cs
+++ Program.cs
@@ -7,3 +7,15 @@ string[] original = { "efew", "ff", "23f4", "4ga", "rew", "fdfd2", "jlo", "tyzs"
 string[] filter = new string[original.Length];
 int i2 = 0;
 
+void CreateFilter()
+{
+    for (int i = 0; i < original.Length; i++)
+    {
+        if (original[i].Length <= 3)
+        {
+            filter[i2] = original[i];
+            i2++;
+        }
+    }
+}
+
